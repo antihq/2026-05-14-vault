63c2169c2746dc384130e8216022ff466cc1805f
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-05-25 19:18:54 -0600

    responsive justify-between on name row, remove truncation

--- resources/views/pages/passwords/⚡index.blade.php
+++ resources/views/pages/passwords/⚡index.blade.php
@@ -57,9 +57,9 @@ new #[Title('Passwords')] class extends Component
                         password: {{ \Illuminate\Support\Js::encode($password->password) }}
                     }"
                 >
-                    <div class="flex flex-wrap justify-between gap-x-3">
-                        <div class="flex flex-1 flex-wrap gap-x-3 items-center min-w-0">
-                            <p class="font-semibold truncate">{{ $password->name }}</p>
+                    <div class="flex flex-wrap sm:justify-between gap-x-3">
+                        <div class="flex flex-wrap gap-x-3 items-center min-w-0">
+                            <p class="font-semibold">{{ $password->name }}</p>
                             <flux:link :href="route('passwords.edit', [$teamModel, $password])" wire:navigate>
                                 Edit
                             </flux:link>
