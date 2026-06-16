bc8f47ce9d6c8bee82c02e9ce57989106eb251fa
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-06-16 14:53:20 -0600

    Align auth and account layouts with app layout

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -3,33 +3,46 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="bg-zinc-50 dark:bg-zinc-900 antialiased text-zinc-950 dark:text-white text-base/6 sm:text-sm/6">
-        <header>
-            <nav class="flex items-end flex-wrap py-5">
-                <div class="lg:w-64 lg:justify-end px-4 flex gap-x-3 flex-wrap">
-                    <a href="{{ route('home') }}" wire:navigate>{{ config('app.name') }}</a>
+    <body class="bg-zinc-50 dark:bg-zinc-900 antialiased text-zinc-800 dark:text-white text-sm/6">
+        <header class="border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-900">
+            <nav class="flex flex-wrap gap-y-1">
+                <div class="lg:w-64 pl-1.5 lg:flex justify-end items-start hidden">
+                    <flux:navbar class="-mb-px">
+                        <flux:navbar.item :href="route('home')" :accent="true" class="text-zinc-800" wire:navigate>{{ config('app.name') }}</flux:navbar.item>
+                    </flux:navbar>
                 </div>
 
-                <div class="flex-1 flex-wrap flex px-4">
-                    <div class="flex gap-x-3">
-                        <flux:link :href="route('dashboard')" class="lowercase" wire:navigate>dashboard</flux:link>
-                    </div>
+                <div class="w-full flex-1 flex-wrap flex px-1.5">
+                    <flux:navbar class="pb-3">
+                        <flux:navbar.item :href="route('home')" :accent="true" class="lg:hidden" wire:navigate>{{ config('app.name') }}</flux:navbar.item>
+                        <flux:navbar.item :href="route('dashboard')" :accent="true" class="text-zinc-800" wire:navigate>Dashboard</flux:navbar.item>
+                    </flux:navbar>
 
                     <div aria-hidden="true" class="flex-1"></div>
 
-                    <div class="flex gap-x-3">
-                        logged in as <flux:link :href="route('settings')" class="lowercase" wire:navigate>{{ Auth::user()->email }}</flux:link>
-                        <form method="POST" action="{{ route('logout') }}" class="inline-flex">
+                    <flux:navbar class="-mb-px pb-3">
+                        <flux:navbar.item :href="route('settings')" wire:navigate :accent="true" class="text-zinc-800 max-lg:hidden">{{ Auth::user()->email }}</flux:navbar.item>
+                        <form method="POST" action="{{ route('logout') }}" class="flex max-lg:hidden">
                             @csrf
-                            <flux:button size="sm" variant="filled" type="submit" class="lowercase">logout</flux:button>
+                            <flux:navbar.item type="submit" class="text-zinc-800">Logout</flux:navbar.item>
                         </form>
-                    </div>
+                        <flux:dropdown class="lg:hidden">
+                            <flux:navbar.item icon="ellipsis-horizontal" class="text-zinc-800" />
+                            <flux:navmenu>
+                                <flux:navmenu.item :href="route('settings')" wire:navigate :accent="true" class="text-zinc-800">{{ Auth::user()->email }}</flux:navmenu.item>
+                                <form method="POST" action="{{ route('logout') }}" class="flex">
+                                    @csrf
+                                    <flux:navmenu.item type="submit" class="text-zinc-800">Logout</flux:navmenu.item>
+                                </form>
+                            </flux:navmenu>
+                        </flux:dropdown>
+                    </flux:navbar>
                 </div>
             </nav>
         </header>
 
         <main class="lg:pl-64">
-            <div class="p-4 pt-0">
+            <div class="px-4 py-6">
                 <div class="w-full max-w-6xl">
                     {{ $slot }}
                 </div>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,37 +3,43 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="bg-zinc-50 dark:bg-zinc-900 antialiased text-zinc-950 dark:text-white text-base/6 sm:text-sm/6">
-        <header>
-            <nav class="flex items-end flex-wrap py-5">
-                <div class="lg:w-64 lg:justify-end px-4 flex gap-x-3 flex-wrap">
-                    <a href="{{ route('home') }}" wire:navigate>{{ config('app.name') }}</a>
+    <body class="bg-zinc-50 dark:bg-zinc-900 antialiased text-zinc-800 dark:text-white text-sm/6">
+        <header class="border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-900">
+            <nav class="flex flex-wrap gap-y-1">
+                <div class="lg:w-64 pl-1.5 lg:flex justify-end items-start hidden">
+                    <flux:navbar class="-mb-px">
+                        <flux:navbar.item :href="route('home')" :accent="true" class="text-zinc-800" wire:navigate>{{ config('app.name') }}</flux:navbar.item>
+                    </flux:navbar>
                 </div>
 
-                <div class="w-full lg:flex-1 flex-wrap flex px-4 gap-x-3 gap-y-1 md:justify-between max-sm:flex-wrap-reverse">
+                <div class="w-full flex-1 flex-wrap flex px-1.5">
+                    <flux:navbar class="pb-3">
+                        <flux:navbar.item :href="route('home')" :accent="true" class="lg:hidden" wire:navigate>{{ config('app.name') }}</flux:navbar.item>
+                    </flux:navbar>
+
                     <div aria-hidden="true" class="flex-1"></div>
 
-                    <div class="flex gap-x-3">
+                    <flux:navbar class="-mb-px pb-3">
                         @guest
                             @if (Route::has('login'))
-                                <flux:link :href="route('login')" :accent="false" class="lowercase whitespace-nowrap" wire:navigate>Sign in</flux:link>
+                                <flux:navbar.item :href="route('login')" :accent="false" class="text-zinc-800" wire:navigate>Sign in</flux:navbar.item>
                             @endif
 
                             @if (Route::has('register'))
-                                <flux:link :href="route('register')" :accent="false" class="lowercase whitespace-nowrap" wire:navigate>Create account</flux:link>
+                                <flux:navbar.item :href="route('register')" :accent="false" class="text-zinc-800" wire:navigate>Create account</flux:navbar.item>
                             @endif
                         @endguest
 
                         @auth
-                            <flux:link :href="route('dashboard')" :accent="false" class="lowercase" wire:navigate>dashboard</flux:link>
+                            <flux:navbar.item :href="route('dashboard')" :accent="false" class="text-zinc-800" wire:navigate>Dashboard</flux:navbar.item>
                         @endauth
-                    </div>
+                    </flux:navbar>
                 </div>
             </nav>
         </header>
 
         <main class="lg:pl-64">
-            <div class="p-4 pt-0">
+            <div class="px-4 py-6">
                 <div class="w-full max-w-6xl">
                     {{ $slot }}
                 </div>
